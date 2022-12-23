301ef0c74bbcea4a15a9375202f8ef02581b44eb
Blai Vilarnau Olid <blvilarn@cbr15s6.42barcelona.com>
2022-12-23 06:59:50 +0100

    minor visual improvements

---
+++ utils/wordcount/wordcount.js
@@ -0,0 +1,13 @@
+function countWords(){
+	text = document.getElementById("wc").value
+	text = text.split(" ");
+	let total = 0;
+	text.forEach(word => {
+		if (word != "") {
+			total++;
+		}
+	});
+	document.getElementById("totalwords").innerHTML = "Total words: " + total;
+}
+
+window.onkeydown = function (){countWords()}
